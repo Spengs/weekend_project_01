05cf4204088911be8e8cd499767f1d983669f73d
Spengs <Jotto465@gmail.com>
2016-08-01 09:54:20 -0500

    tweaked stuff

--- app.js
+++ app.js
@@ -3,41 +3,41 @@ $(document).ready(function() {
     var monthlySalary = 0;
     var totalSalary = 0;
     $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
+        event.preventDefault();
 
-      // initialize a new variable as an empty object
-      var values = {};
+        // initialize a new variable as an empty object
+        var values = {};
 
-      // convert the form inputs into an array
-      var fields = $('#employeeinfo').serializeArray();
-      // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
-      fields.forEach(function(element, index, array) {
-        // review index notation vs. dot notation on objects
-        // here, dot notation wouldn't work
-        values[element.name] = element.value;
+        // convert the form inputs into an array
+        var fields = $('#employeeinfo').serializeArray();
+        // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
+        fields.forEach(function(element, index, array) {
+            // review index notation vs. dot notation on objects
+            // here, dot notation wouldn't work
+            values[element.name] = element.value;
 
-      });
+        });
 
-      // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
+        // clear out inputs
+        $('#employeeinfo').find('input[type=text]').val('');
 
 
-      // append to DOM
-      appendDom(values);
-      console.log(values);
+        // append to DOM
+        appendDom(values);
+        console.log(values);
     });
-    $("#nix").on("click", function(){
+    $("#nix").on("click", function() {
       $('#container').children().last().remove();
-    })
+    });
 
     function appendDom(empInfo) {
-      empInfo.employeesalary = parseInt(empInfo.employeesalary);
-      monthlySalary = empInfo.employeesalary / 12;
-      totalSalary = totalSalary + monthlySalary;
+        empInfo.employeesalary = parseInt(empInfo.employeesalary);
+        monthlySalary = empInfo.employeesalary / 12;
+        totalSalary = totalSalary + monthlySalary;
 
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
-      $el.append('<p>' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + ' ' + empInfo.employeenumber + ' ' + empInfo.employeetitle + ' ' + empInfo.employeesalary + ' Salary Cost per Month = ' + totalSalary  + '</p>');
+        $('#container').append('<div class="person"></div>');
+        var $el = $('#container').children().last();
+        $el.append('<p>' + "Name: " + empInfo.employeefirstname + ' ' + empInfo.employeelastname + ' | ' + "Number: " + empInfo.employeenumber + ' | ' + "Title: " + empInfo.employeetitle + ' | ' + "Salary: " + empInfo.employeesalary + ' | Company Salary Cost per Month = ' + totalSalary + '</p>');
 
     }
 
